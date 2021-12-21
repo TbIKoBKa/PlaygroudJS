dd1a15c80512ef2c50b7a8bdde1feea2d5811f49
Eugene Mormul <zhenya.mormul@gmail.com>
2021-12-21 15:48:43 +0200

    remove mock data

--- src/bus/snacks/slice.ts
+++ src/bus/snacks/slice.ts
@@ -7,33 +7,7 @@ import * as types from './types';
 // Reducers
 import * as reducers from './reducers';
 
-const initialState: types.SnacksState = [
-    {
-        title: '1',
-        type:  'info',
-        id:    '1',
-    },
-    {
-        title: '1',
-        type:  'warn',
-        id:    '2',
-    },
-    {
-        title: '1',
-        type:  'error',
-        id:    '3',
-    },
-    {
-        title: '1',
-        type:  'info',
-        id:    '4',
-    },
-    {
-        title: '1',
-        type:  'success',
-        id:    '5',
-    },
-];
+const initialState: types.SnacksState = null;
 
 export const snacksSlice = createSlice<types.SnacksState, typeof reducers>({
     name: 'snacks',
